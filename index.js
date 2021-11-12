a87d02c5e1ae0ec3f86affc60f44155c725f92b9
Carlos Solano <solanoandy@gmail.com>
2021-11-12 17:27:55 -0600

    solved prototypes 1-3

--- index.js
+++ index.js
@@ -157,15 +157,19 @@ function greeting(firstName, lastName){
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
  Use the constructor function named CuboidMaker to accept properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker(){
-  /*Your Code Here */
+function CuboidMaker(object){
+  this.length = object.length;
+  this.width = object.width;
+  this.height = object.height;
 }
 
 
 /* 🐴🐴🐴 Step 2: Volume Method 🐴🐴🐴
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height   */
-
+  CuboidMaker.prototype.volume = function(){
+    return this.length*this.width*this.height;
+  }
 
 
 
@@ -175,7 +179,9 @@ function CuboidMaker(){
   Formula for cuboid surface area of a cube: 
   2 * (length * width + length * height + width * height)  */
 
-
+CuboidMaker.prototype.surfaceArea = function(){
+  return 2*(this.length*this.width + this.length*this.height + this.width*this.height);
+}
 
 
 
